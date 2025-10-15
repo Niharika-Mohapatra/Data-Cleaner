d8a962071d3de483c66b974cd49f037233c04429
Niharika Mohapatra <niharikamohapatra922@gmail.com>
2025-10-15 22:52:36 +0530

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -107,7 +107,8 @@ if uploaded_files:
         else:
             columns = []
             for df in datasets:
-                columns.append(df.columns)  
+                for key, value in df:
+                    columns.append(value)
             stacked_df = pd.concat(datasets, axis=1)
             st.write(columns)
           
